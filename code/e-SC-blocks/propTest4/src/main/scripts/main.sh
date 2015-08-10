ddda190da2bdb96a9d7ddae2d064d6530e669b4e
wng1 <w.ng1@ncl.ac.uk>
2015-08-10 01:08:28 +0100

    test output - plot

--- code/e-SC-blocks/propTest4/src/main/scripts/main.sh
+++ code/e-SC-blocks/propTest4/src/main/scripts/main.sh
@@ -12,34 +12,12 @@ for f in "${INPUT__input_files__LIST[@]}" ; do
     echo "$f" >> $OUTPUT__output_files
 done
 
-OUTFILE=$( mktemp output-XXXX.csv )
+OUTFILE=$( mktemp output-XXXX.txt )
 echo $OUTFILE >> $OUTPUT__output_properties
-#source $INPUT__input_files
 
-echo "$PROP__u=jcala" >> $OUTPUT__u
-echo "$PROP__p=password1" >> $OUTPUT__p
-echo "$PROP__H=Host1" >> $OUTPUT__H
-echo "$PROP__P=Port1" >> $OUTPUT__P
-
-echo "$PROP__u=jcala" >> $OUTFILE
-echo "$PROP__p=password" >> $OUTFILE
-echo "$PROP__H=Host" >> $OUTFILE
-echo "$PROP__P=Port" >> $OUTFILE
-
-
-
-
-
-#cho "$INPUT__input_files" >> $OUTPUT__output_properties  # Import properties to the block
-#at "$INPUT__input_files" >> $OUTPUT__output_properties  # Copy properties to the block
-
-
-
-#$DEP__OphidiaTerminal__oph_term oph_term -u $1 $PROP__u -p $2 $PROP__p -H $3 $PROP__H -P $4 $PROP__P -e "oph_createcontainer container=$PROP__container;dim=$PROP__dim;dim_type=$PROP__dim_type;hierarchy=$PROP__hierarchy;" | tee -a "$OUTFILE"
-
-
-
-# echo "#!/bin/bash" > input2.txt
+for f in $BLOCK_HOME/scripts/output-*.txt; do
+cat $INPUT__input_files > "$OUTFILE"
+done
 
 # INPUTFILE=$BLOCK_HOME/scripts/input2.txt
 # echo $INPUTFILE >> $OUTPUT__output_properties
@@ -55,4 +33,23 @@ echo "$PROP__P=Port" >> $OUTFILE
 # else
 # echo "not done"
 # fi
+
+#source $INPUT__input_files
+
+#echo "$PROP__u=jcala" >> $OUTPUT__u
+#echo "$PROP__p=password1" >> $OUTPUT__p
+#echo "$PROP__H=Host1" >> $OUTPUT__H
+#echo "$PROP__P=Port1" >> $OUTPUT__P
+
+#echo "$PROP__u=jcala" >> $OUTFILE
+#echo "$PROP__p=password" >> $OUTFILE
+#echo "$PROP__H=Host" >> $OUTFILE
+#echo "$PROP__P=Port" >> $OUTFILE
+
+#echo "$INPUT__input_files" >> $OUTPUT__output_properties  # Import properties to the block
+#cat "$INPUT__input_files" >> $OUTPUT__output_properties  # Copy properties to the block
+
+#$DEP__OphidiaTerminal__oph_term oph_term -u $1 $PROP__u -p $2 $PROP__p -H $3 $PROP__H -P $4 $PROP__P -e "oph_createcontainer container=$PROP__container;dim=$PROP__dim;dim_type=$PROP__dim_type;hierarchy=$PROP__hierarchy;" | tee -a "$OUTFILE"
+
+# echo "#!/bin/bash" > input2.txt
 
