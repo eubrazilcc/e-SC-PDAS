31c86b1ea1a67507b258364498d12d2fa512933a
wng1 <w.ng1@ncl.ac.uk>
2015-08-16 00:50:27 +0100

    Update
    minor tweaks

--- code/e-SC-blocks/propTest4/src/main/scripts/main.sh
+++ code/e-SC-blocks/propTest4/src/main/scripts/main.sh
@@ -28,7 +28,7 @@ done
 # fi
 
 ###### ImportCube.sh
-function checkCubeName
+function checkCubeName()
 {
 if [ "$PROP__cubeName" == "$PROP__container" ]
 then
@@ -43,19 +43,16 @@ fi
 
 checkCubeName
 
-function checkParameters
+function checkParameters()
 {
-var_mandatory="container=$PROP__container;host_partition=$PROP__host_partition;cwd=$PROP__cwd;measure=$PROP__measure;src_path=$PROP__scr_path;exp_dim=$PROP__exp_dim;imp_dim=$PROP__imp_dim;"
-var_non_mandatory="sessionid=$PROP__sessionid;ncores=$PROP__ncores;exec_mode=$PROP__exec_mode;ioserver=$PROP__ioserver;import_metadata=$PROP__import_metadata;schedule=$PROP__schedule;nhost=$PROP__nhosts;ndbms=$PROP__ndbms;ndb=$PROP__ndb;nfrag=$PROP__nfrag;run=$PROP__run;subset_dims=$PROP__subset_dims;subset_type=$PROP__subset_type;subset_filter=$PROP__subset_filter;exp_concept_level=$PROP__exp_concept_level;imp_concept_level=$PROP__imp_concept_level;compressed=$PROP__compressed;grid=$PROP__grid;objkey_filter=$PROP__objkey_filter;"
+check="$PROP__container $PROP__host_partition $PROP__cwd $PROP__measure $PROP__scr_path $PROP__exp_dim $PROP__imp_dim"
 
-var_parameters="$var_mandatory $var_non_mandatory"
-
-for parameter in $var_mandatory
+for parameter in $check
 do
 
-if [ "${parameter}" == "" ]
+if [ "${parameter}" != "" ]
 then
-  $DEP__OphidiaTerminal__oph_term oph_term -j -u $1 $PROP__u -p $2 $PROP__p -H $3 $PROP__H -P $4 $PROP__P -e "oph_importnc $mandatory_para $non_mandatory_para"
+  importNC
 exit 0
 else
   echo "You need to fill in all the mandatory parameters required."
@@ -63,4 +60,13 @@ exit 1
 fi
 done
 }
+checkParameters
 
+function importNC()
+{
+var_mandatory="container=$PROP__container;host_partition=$PROP__host_partition;cwd=$PROP__cwd;measure=$PROP__measure;src_path=$PROP__scr_path;exp_dim=$PROP__exp_dim;imp_dim=$PROP__imp_dim;"
+var_non_mandatory="sessionid=$PROP__sessionid;ncores=$PROP__ncores;exec_mode=$PROP__exec_mode;ioserver=$PROP__ioserver;import_metadata=$PROP__import_metadata;schedule=$PROP__schedule;nhost=$PROP__nhosts;ndbms=$PROP__ndbms;ndb=$PROP__ndb;nfrag=$PROP__nfrag;run=$PROP__run;subset_dims=$PROP__subset_dims;subset_type=$PROP__subset_type;subset_filter=$PROP__subset_filter;exp_concept_level=$PROP__exp_concept_level;imp_concept_level=$PROP__imp_concept_level;compressed=$PROP__compressed;grid=$PROP__grid;objkey_filter=$PROP__objkey_filter;"
+var_parameters="$var_mandatory $var_non_mandatory"
+
+  $DEP__OphidiaTerminal__oph_term oph_term -j -u $1 $PROP__u -p $2 $PROP__p -H $3 $PROP__H -P $4 $PROP__P -e "oph_importnc $mandatory_para $non_mandatory_para"
+}
