557e19f7cfdb7bd3c90c05fc07cfc56af189f9e0
wng1 <w.ng1@ncl.ac.uk>
2015-08-28 11:29:47 +0100

    Final Update Apply.sh
    Submitted

--- code/e-SC-blocks/propTest4/src/main/scripts/Ophidia_Apply.sh
+++ code/e-SC-blocks/propTest4/src/main/scripts/Ophidia_Apply.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+#!/bin/bash
+#
+# NOTED:CHANGED FOR TESTING PURPOSES  
+#
+# Block Name: Ophidia Apply
+# Author: William Ng
+#
+# ------------------------------------------------------------
+# The following environment variables are required to connect
+# ------------------------------------------------------------
+
+echo "u=$PROP__u" >> $OUTPUT__output_properties
+echo "p=$PROP__p" >> $OUTPUT__output_properties
+echo "H=$PROP__H" >> $OUTPUT__output_properties
+echo "P=$PROP__P" >> $OUTPUT__output_properties
+echo "container=$PROP__container" >> $OUTPUT__output_properties
+
+if [ "$PROP__cube" == "" ];
+	then
+echo "Cannot execute without cube identifer"
+		exit 1
+	else 
+		echo ""
+fi
+
+OUTFILE=$(mktemp apply-XXXX.csv)
+echo $OUTFILE >> $OUTPUT__output_files
+
+FILE=(apply-*.csv)
+
+for f in $BLOCK_HOME/scripts/apply-*.csv; do
+
+if [ -f "$FILE" ];  
+then
+
+PROP__cube=$(cat $INPUT__input_files | jq -c -r '.response.response[0].objcontent[0].message' | sed 's/[][]//g' )
+$DEP__OphidiaTerminal__oph_term oph_term -j -u $1 $PROP__u -p $2 $PROP__p -H $3 $PROP__H -P $4 $PROP__P -e "oph_apply query=$PROP__query;cube=$PROP__cube;" | tee -a "$OUTFILE"
+
+else
+	echo "Not Completed."
+fi
+done
 
 if [ "$PROP__u" != "" ] && [ "$PROP__p" != "" ] && [ "$PROP__H" != "" ] && [ "$PROP__P" != "" ];
 then
